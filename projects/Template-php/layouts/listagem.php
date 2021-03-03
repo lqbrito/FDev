cb1a29c63dab4317e267e2fd761f292580ac24e0
Luciano Brito Querido <lqbrito@email.com>
2021-03-03 09:48:34 -0300

    03/03/2021 09:50

--- projects/Template-php/layouts/listagem.php
+++ projects/Template-php/layouts/listagem.php
@@ -1,10 +1,10 @@
 <?php
-    include_once('../parent/view.php');
+include_once('../parent/view.php');
 
-    class listagemView extends View
+class listagemView extends View
+{
+    public function view($data)
     {
-        public function view($data)
-        {
             $pag = 0; // Use esta numeração para representar o crud atual e indicar sua opção de menu como ativa
             $[nome_model] = $data['[nome_model]']; // Esta variável representa a model passada para a página
             $listaTudo = $data['listaTudo']; // Indica se é para listar todos os registros da tabela
@@ -13,6 +13,8 @@
             $pagina = $data['pagina']; // Página atual da tabela em caso de paginação
             $paginas = $data['paginas']; // Total de páginas da tabela em caso de paginação
             $titulo = "Cadastro de [nome_model]"; // Título da página
+            $empresa = APP_EMPRESA;
+            $mensagem = APP_MENSAGEM;
             $campoBusca = "uma descrição"; // Placeholder para o campo de busca na tabela
             $controller = "[nome_controller].php"; // Nome do controller para retornar da página
             // Cabeçalho comum a todas as páginas
@@ -24,22 +26,21 @@
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
-                            <?php echo $titulo; ?>
+                            <div class="col-md-9 text-left">
+                                <?php echo $titulo; ?>
+                            </div>
+                            <div class="col-md-3 text-right">
+                                <div class="btn-group" role="group" aria-label="Basic example">
+                                    <a href='../public' class="btn btn-sm btn-outline-primary mr-1"><i class="fas fa-reply"></i> Voltar</a>
+                                    <form action='../controllers/[nome_model].php' method="post">
+                                        <input type = "hidden" name = "operacao" value = "form/incluir">
+                                        <button type="submit" class="btn btn-sm btn-outline-primary"><i class="fas fa-plus"></i> Incluir</button>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
 
                         <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-12 text-right">
-                                    <div class="btn-group" role="group" aria-label="Basic example">
-                                        <a href='../public' class="btn btn-sm btn-primary mr-1"><i class="fas fa-reply"></i> Voltar</a>
-                                        <form action='../controllers/[nome_model].php' method="post">
-                                            <input type = "hidden" name = "operacao" value = "form/incluir">
-                                            <button type="submit" class="btn btn-sm btn-primary"><i class="fas fa-plus"></i> Incluir</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                                                
                             <div class="table-responsive mt-3">
                                 <table class="table table-striped table-hover table-sm table-condensed">
                                     <thead class="thead-dark">
@@ -53,7 +54,7 @@
                                         <?php 
                                         foreach($[nome_model] as $tp)
                                         {                                   
-                                        ?>
+                                            ?>
                                             <tr>
                                                 [nome_campo]
                                                 <td>
@@ -76,7 +77,7 @@
                                                     </div>
                                                 </td>
                                             </tr>
-                                        <?php 
+                                            <?php 
                                         }
                                         ?>
                                     </tbody>
@@ -84,20 +85,20 @@
 
                                 <?php 
                                     if (!$listaTudo) // Verifica se é para fazer a paginação dos dados
-                                        $this->links($controller, $pagina, $paginas);
-                                ?>
-                                
-                            </div>
-                        
-                        </div>
+                                    $this->links($controller, $pagina, $paginas);
+                                    ?>
 
+                                </div>
+
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <?php
+                <?php
             // Rodapé comum a todas as páginas
-            include_once ("../views/layouts/" . $_SESSION['app_ui'] . "_rodape.php");
+                include_once ("../views/layouts/" . $_SESSION['app_ui'] . "_rodape.php");
+            }
         }
-    }
-?>
+        ?>
